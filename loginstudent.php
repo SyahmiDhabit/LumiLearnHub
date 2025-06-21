be7f113298b00201c84602c79ac892e75fbdadd5
SyahmiDhabit <d032310235@student.utem.edu.my>
2025-06-21 23:00:20 +0800

    Update loginstudent.php

--- loginstudent.php
+++ loginstudent.php
@@ -17,9 +17,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($result && $result->num_rows === 1) {
         $row = $result->fetch_assoc();
 
-        if (password_verify($student_password, $student['student_password'])) {
-            $_SESSION['student_id'] = $student['studentID'];
-            $_SESSION['student_fullname'] = $student['student_fullName'];
+        // Semak password
+        if (password_verify($password, $row['student_password'])) {
+            // Simpan maklumat dalam session
+            $_SESSION['student_username'] = $row['student_username'];
+
+            // Redirect ke student interface
             header("Location: studentinterface.php");
             exit();
         } else {
