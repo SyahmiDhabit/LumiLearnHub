4511f5a865398b93ab3605cc86ffba317ecf9661
SyahmiSaidi <syamie909@gmail.com>
2025-06-22 00:48:08 +0800

    update tutorinterface.php

--- tutorinterface.php
+++ tutorinterface.php
@@ -23,21 +23,9 @@ $tutorFullname = $_SESSION['tutor_fullname'];
 </head>
 <body>
   <div class="header">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    <div class="brand">LumiLearnHub</div>
-    <div class="welcome">WELCOME TUTOR!</div>
-    <a href="profiletutor.php" class="profile-icon"></a>
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     <a href="tutorinterface.html" class="brand">LumiLearnHub</a>
     <div class="welcome">WELCOME, <?php echo strtoupper($tutorFullname); ?>!</div>
     <div class="profile-icon"></div>
->>>>>>> Stashed changes
   </div>
 
   <div class="container">
